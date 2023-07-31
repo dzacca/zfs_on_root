f121074eaf86c6ad722f52c78f37f9c5ce7eedc9
Diego Zaccariotto <diego@zaccariotto.net>
2023-07-31 14:59:21 +0200

    merged chroot scripts

--- README.md
+++ README.md
@@ -9,23 +9,7 @@ gsettings set org.gnome.desktop.media-handling automount false
 sudo -i
 
 from_live_cd.sh
-from_chroot1.sh
-
-vi /etc/default/grub
-``` shell
-# Add init_on_alloc=0 to: GRUB_CMDLINE_LINUX_DEFAULT
-# Save and quit (or see the next step).
-
-vi /etc/default/grub
-# Comment out: GRUB_TIMEOUT_STYLE=hidden
-# Set: GRUB_TIMEOUT=5
-# Below GRUB_TIMEOUT, add: GRUB_RECORDFAIL_TIMEOUT=5
-# Remove quiet and splash from: GRUB_CMDLINE_LINUX_DEFAULT
-# Uncomment: GRUB_TERMINAL=console
-# Save and quit.
-```
-
-from_chroot2.sh
+from_chroot.sh
 from_live_cd2.sh
 first_boot.sh
 
--- from_chroot.sh
+++ from_chroot.sh
@@ -37,3 +37,20 @@ grub-probe /boot
 
 update-initramfs -c -k all
 
+sed -i 's/^GRUB_TIMEOUT=0/GRUB_TIMEOUT=5/g' /etc/default/grub
+sed -i '/GRUB_TIMEOUT=/a GRUB_RECORDFAIL_TIMEOUT=5' /etc/default/grub
+sed -i 's/"quiet splash"/"quite splash init_on_alloc=0"/g' /etc/default/grub
+sed -i 's/^#GRUB_TERMINAL/GRUB_TERMINAL/g' /etc/default/grub  
+sync
+update-grub
+grub-install --target=x86_64-efi --efi-directory=/boot/efi \
+    --bootloader-id=ubuntu --recheck --no-floppy
+mkdir /etc/zfs/zfs-list.cache
+touch /etc/zfs/zfs-list.cache/bpool
+touch /etc/zfs/zfs-list.cache/rpool
+zed -F &
+sleep 3
+
+sed -Ei "s|/mnt/?|/|" /etc/zfs/zfs-list.cache/*
+# need to exit from chroot now, temporarily disabled while testing the script
+#exit
