f4d9947c1b5410b3940016c0cd87b20ab23b5b22
Diego Zaccariotto <diego@zaccariotto.net>
2023-07-31 18:46:33 +0200

    fixed 4

--- 4_first_boot.sh
+++ 4_first_boot.sh
@@ -21,8 +21,8 @@ apt install --yes ubuntu-desktop
 rm /etc/netplan/01-netcfg.yaml
 
 echo "network:" >/etc/netplan/01-network-manager-all.yaml
-echo"  version: 2" >>/etc/netplan/01-network-manager-all.yaml
-echo"  renderer: NetworkManager" >>/etc/netplan/01-network-manager-all.yaml
+echo "  version: 2" >>/etc/netplan/01-network-manager-all.yaml
+echo "  renderer: NetworkManager" >>/etc/netplan/01-network-manager-all.yaml
 
 for file in /etc/logrotate.d/* ; do
     if grep -Eq "(^|[^#y])compress" "$file" ; then
