7fde5009174b4e006da332193333f8fc33354185
Diego Zaccariotto <diego@zaccariotto.net>
2023-12-08 13:59:19 +0100

    bug fix

--- zfs_on_root_zbm.sh
+++ zfs_on_root_zbm.sh
@@ -469,6 +469,7 @@ install_ubuntu() {
 
     if [[ ${DISTRO} != "server" ]];
 		then
+      mkdir -p /"${MOUNTPOINT}"/var/lib/AccountsService
 			zfs create 	"${POOLNAME}"/ROOT/"${ID}"/var/lib/AccountsService
     fi
 
